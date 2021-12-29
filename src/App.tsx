b79aacd8982f3e1c00d4ec80225cfc94e67c2c90
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-12-30 01:05:05 +0200

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,41 @@
 import './App.scss';
 import React, { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router';
 
-function App() {
+import Oracle from './components/Oracle';
+import { data } from './data';
+import OracleCells from './components/OracleCells';
+
+export interface OracleType {
+  id: number,
+    oracleNumber: number | string,
+    text: string,
+    link?: string,
+}
+
+export const App: React.FC = () => {
+  const [ selectedOracle, setSelectedOracle ] = useState<OracleType | null>();
+
+  const chooseOracle = (oracleFromEvent: OracleType): void => {
+    setSelectedOracle(oracleFromEvent);
+  }
+
+  const onClose = () => {
+    setSelectedOracle(null);
+  }
+
   return (
-    <div className="App">Hello goy
+    <div className="App">
+      <div className="App__h">Знайди номер свого передбачення нижче і натискай!</div>
+      <OracleCells onSelect={chooseOracle} />
+      {selectedOracle && (
+          <Routes>
+            <Route
+              path={`/${selectedOracle.id}`}
+              element={(<Oracle onClose={onClose} oracle={selectedOracle} />)}
+            />
+          </Routes>
+          )}
     </div>
   );
 }
